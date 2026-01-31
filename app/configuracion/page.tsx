2ff5471abc2a58c1ee9e77cd38810a1a23f5ce38
ArgenisBouzas <argenisjb@gmail.com>
2026-01-31 08:55:43 -0400

    intento de correccion 31/1/2026 8.55am

---
+++ app/configuracion/page.tsx
@@ -0,0 +1,11 @@
+export default function ConfiguracionPage() {
+  return (
+    <div className="p-8 flex flex-col gap-6">
+      <h1 className="text-2xl font-bold mb-4">Configuración</h1>
+      <p>Aquí puedes ajustar las configuraciones de tu sistema de facturación.</p>
+      <p>en desarrollo</p>
+      <p className="text-5xl text-gray-500">ARGENIS BOUZAS</p>
+      {/* Agrega más contenido de configuración según sea necesario */}
+    </div>
+  );
+}
